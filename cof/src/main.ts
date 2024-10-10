012c4d9df3ca1b99c794973bc214532616cd230f
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-10-10 23:38:43 +0200

    fixing reloading battery game feature

--- cof/src/main.ts
+++ cof/src/main.ts
@@ -2,6 +2,14 @@ import "./style.css";
 
 let BatteryCharge: number = 0;
 let maxCharge: number = 6;
+// let isGameStarted = false;
+// let puntoCaricamentoBatteria: number = 0;
+
+const util = {
+    isGameStarted: false,
+    puntoCaricamentoBatteria: 0,
+};
+
 document.addEventListener("DOMContentLoaded", () => {
     // creo la griglia di gioco e la preparo con classi e compagnia bella.
     const main = document.getElementById("main");
@@ -10,7 +18,8 @@ document.addEventListener("DOMContentLoaded", () => {
         createAside(main);
         createGriglia(main);
         createBatteryContainer(main);
-        reloadBattery();
+        createMessagesContainer(main);
+        welcomeMessage();
     }
 });
 
@@ -33,6 +42,59 @@ function createBoxes(aside: HTMLElement) {
     }
 }
 
+function showWeaponsInAside(box: HTMLElement, i: number) {
+    const vetrina = document.createElement("div");
+    vetrina.classList.add(`vetrina`);
+    const nome = document.createElement("h4");
+    nome.classList.add("stileNome");
+    vetrina.appendChild(nome);
+    switch (i) {
+        case 0:
+            nome.innerHTML = `🚀 Missle 🚀`;
+            createWepon(vetrina, "./imgs/missle.png");
+            weaponCost(vetrina, 2);
+            break;
+        case 1:
+            nome.innerHTML = ` 🧬 Laser 🧬 `;
+            createWepon(vetrina, "./imgs/laser.png");
+            weaponCost(vetrina, 2);
+            break;
+        case 2:
+            nome.innerHTML = `⚔ soldato ⚔`;
+            createWepon(vetrina, "./imgs/solder.png");
+            weaponCost(vetrina, 4);
+            break;
+        case 3:
+            nome.innerHTML = ` arma4 `;
+            break;
+
+        default:
+            null;
+            break;
+    }
+
+    box.appendChild(vetrina);
+}
+
+function createWepon(vetrina: HTMLElement, path: string) {
+    const weapon: HTMLImageElement = document.createElement("img");
+    weapon.src = path;
+    weapon.classList.add("imgCompact");
+    vetrina.appendChild(weapon);
+}
+
+function weaponCost(vetrina: HTMLElement, batteryCost: number) {
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("wrapperBatteryCost");
+    for (let i = 0; i < batteryCost; i++) {
+        const box = document.createElement("div");
+        box.classList.add("batteryCost");
+        // vetrina.appendChild(box);
+        wrapper.appendChild(box);
+    }
+    vetrina.appendChild(wrapper);
+}
+
 // crea la griglia contenente tutte le celle necessarie al gioco
 function createGriglia(main: HTMLElement) {
     const cellContainer = document.createElement("div");
@@ -53,55 +115,6 @@ function createGriglia(main: HTMLElement) {
     main.appendChild(cellContainer);
 }
 
-function showWeaponsInAside(box: HTMLElement, i: number) {
-    const vetrina = document.createElement("div");
-    vetrina.classList.add(`vetrina`);
-    const nome = document.createElement("h4");
-    nome.classList.add("stileNome");
-    vetrina.appendChild(nome);
-    switch (i) {
-        case 0:
-            nome.innerHTML = `🚀 Missle 🚀`;
-            createRocket(vetrina);
-            rocketCost(vetrina);
-            break;
-        case 1:
-            nome.innerHTML = ` arma2 `;
-            break;
-        case 2:
-            nome.innerHTML = ` arma3 `;
-            break;
-        case 3:
-            nome.innerHTML = ` arma4 `;
-            break;
-
-        default:
-            null;
-            break;
-    }
-
-    box.appendChild(vetrina);
-}
-
-function createRocket(vetrina: HTMLElement) {
-    const rocket = document.createElement("img");
-    rocket.src = "./imgs/missle.png";
-    rocket.classList.add("imgCompact");
-    vetrina.appendChild(rocket);
-}
-
-function rocketCost(vetrina: HTMLElement) {
-    const wrapper = document.createElement("div");
-    wrapper.classList.add("wrapperBatteryCost");
-    for (let i = 0; i < 2; i++) {
-        const box = document.createElement("div");
-        box.classList.add("batteryCost");
-        // vetrina.appendChild(box);
-        wrapper.appendChild(box);
-    }
-    vetrina.appendChild(wrapper);
-}
-
 function createBatteryContainer(main: HTMLElement) {
     const batteryContainer = document.createElement("div");
     batteryContainer.classList.add("batteryContainer");
@@ -119,12 +132,60 @@ function createSlotBattery(batteryContainer: HTMLElement) {
     }
 }
 
+function createMessagesContainer(main: HTMLElement) {
+    const messageBox = document.createElement("section");
+    messageBox.classList.add("messagebox");
+    main.appendChild(messageBox);
+}
+
+function welcomeMessage() {
+    const messageBox = document.querySelector(".messagebox");
+    const startButton = document.createElement("button");
+    startButton.classList.add("button");
+    startButton.innerHTML = "iniziamo!";
+    const welcome = document.createElement("h4");
+    welcome.classList.add("h4Style");
+    welcome.classList.add("welcome");
+    welcome.innerHTML =
+        "benvenuto al mio gioco di controlla il campo di battaglia! Spero funzioni... sei pronto per partire ? ";
+
+    messageBox?.appendChild(welcome);
+    messageBox?.appendChild(startButton);
+
+    // PUNTO PARTENZA PER GESTIRE AVVIO/STOP DEL GIOCO
+    startButton.addEventListener("click", changeStatusGame);
+}
+
+function changeStatusGame() {
+    if (!util.isGameStarted) {
+        util.isGameStarted = true;
+        reloadBattery();
+        console.log("gioco iniziato");
+        return;
+    }
+
+    if (util.isGameStarted) {
+        util.isGameStarted = false;
+        console.log("gioco interrotto");
+        return;
+    }
+
+    startTimer();
+}
+
 function reloadBattery() {
     let slots = document.querySelectorAll(".slot");
-    let currentSlot = 0; // Contatore per gli slot già riempiti
+    let currentSlot = util.puntoCaricamentoBatteria; // Usa puntoCaricamentoBatteria o parte da 0
 
     let interval = setInterval(() => {
+        if (!util.isGameStarted) {
+            util.puntoCaricamentoBatteria = currentSlot;
+            clearInterval(interval);
+            return;
+        }
+
         if (currentSlot < slots.length) {
+            console.log(currentSlot, "currslot");
             let slot = slots[currentSlot]; // Seleziona lo slot corrente
             slot.classList.add("fillSlot"); // Aggiungi la classe solo a questo slot
             currentSlot++; // Passa allo slot successivo
@@ -134,4 +195,9 @@ function reloadBattery() {
             clearInterval(interval); // Ferma l'interval una volta che tutti gli slot sono pieni
         }
     }, 2000); // Ogni 2 secondi
+}
+
+function startTimer() {
+    const welcomeMessage = document.querySelector(".welcome");
+    welcomeMessage?.classList.add("d-none");
 }
