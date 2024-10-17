cb87c3878ee42192fb067d34f0983c8f2dfca6e3
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-10-17 22:15:39 +0200

    fix pacman effect

--- cof/src/PATCH/removePacManEffect.ts
+++ cof/src/PATCH/removePacManEffect.ts
@@ -4,13 +4,16 @@ import { util } from "../main";
 export async function PATCH_adiacentCellToBorderCell() {
     // return new Promise((res, rej) => {
     const esiti = [];
+
+    // il valore util.raggioAzioneMissile[0] corrisponde alla cella cliccata col mouse, trovo le altre a partire da questa.
     let centerCellValue = util.raggioAzioneMissile[0];
     console.log(centerCellValue, "cella centrale ");
     // const centerCell = document.querySelector(`${centerCellValue}`);
-    const adiacentCellDx = document.querySelector(`.c${parseInt(centerCellValue.slice(1) + 1).toString()}`);
+    const adiacentCellDx = document.querySelector(`.c${(parseInt(centerCellValue.slice(1)) + 1).toString()}`);
     const adiacentCellSn = document.querySelector(`.c${(parseInt(centerCellValue.slice(1)) - 1).toString()}`);
 
     if (adiacentCellSn?.classList.contains("b")) {
+        console.log("sono qui adiacentCellSn");
         let stringValNoLett = centerCellValue.slice(1);
         let numValue = parseInt(stringValNoLett);
 
@@ -42,6 +45,7 @@ export async function PATCH_adiacentCellToBorderCell() {
     }
 
     if (adiacentCellDx?.classList.contains("b")) {
+        console.log("sono qui adiacentCellDx");
         let stringValNoLett = centerCellValue.slice(1);
         let numValue = parseInt(stringValNoLett);
         const otherSideCell_Center = document.querySelector(`.c${numValue + 3}`);
@@ -49,10 +53,22 @@ export async function PATCH_adiacentCellToBorderCell() {
         const otherSideCell_Top = document.querySelector(`.c${numValue + 46}`);
         const otherSideCell_Bottom = document.querySelector(`.c${numValue - 40}`);
 
-        otherSideCell_Center && otherSideCell_Center.classList.remove("blue", "flip-cell");
-        adicentCell_center_plus_2 && adicentCell_center_plus_2.classList.remove("blue", "flip-cell");
-        otherSideCell_Top && otherSideCell_Top.classList.remove("blue", "flip-cell");
-        otherSideCell_Bottom && otherSideCell_Bottom.classList.remove("blue", "flip-cell");
+        if (otherSideCell_Center) {
+            otherSideCell_Center.classList.remove("blue", "flip-cell");
+            otherSideCell_Center.classList.add("red");
+        }
+        if (adicentCell_center_plus_2) {
+            adicentCell_center_plus_2.classList.remove("blue", "flip-cell");
+            adicentCell_center_plus_2.classList.add("red");
+        }
+        if (otherSideCell_Top) {
+            otherSideCell_Top.classList.remove("blue", "flip-cell");
+            otherSideCell_Top.classList.add("red");
+        }
+        if (otherSideCell_Bottom) {
+            otherSideCell_Bottom.classList.remove("blue", "flip-cell");
+            otherSideCell_Bottom.classList.add("red");
+        }
 
         esiti.push(true);
     } else {
