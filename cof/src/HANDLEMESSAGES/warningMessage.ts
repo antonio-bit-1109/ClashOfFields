9ec680747bbb8df66650c6f1c56f261f398474ad
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-10-20 11:48:56 +0200

    add stile e suono su warning message

--- cof/src/HANDLEMESSAGES/warningMessage.ts
+++ cof/src/HANDLEMESSAGES/warningMessage.ts
@@ -1,4 +1,8 @@
+// import warningSoundMsg from ""
+
 export function giveWarningMessage(string: string) {
+    const soundMessage = new Audio("../../public/sounds/soundMessage.mp3");
+    soundMessage.play();
     const warning_Par = document.querySelector(".warning");
     if (warning_Par) {
         warning_Par.classList.add("vibrate");
@@ -7,6 +11,10 @@ export function giveWarningMessage(string: string) {
         setTimeout(() => {
             warning_Par.classList.add("d-none");
             warning_Par.classList.remove("vibrate");
+            warning_Par.classList.add("bigger");
         }, 1000);
+        setTimeout(() => {
+            soundMessage.pause();
+        }, 500);
     }
 }
